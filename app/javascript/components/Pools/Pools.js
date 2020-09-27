e874f306e08e3aff07617879e877dbfe3a22e4fe
Peter <peter@ptorrsmith.com>
2020-09-27 18:53:48 +1300

    yarn add axios, use to get Pools API data

--- app/javascript/components/Pools/Pools.js
+++ app/javascript/components/Pools/Pools.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
+// stateless component
+// const Pools = () => {
+// return <div>This is the Pools#index view for our app</div>
+// }
 const Pools = () => {
-	return <div>This is the Pools#index view for our app</div>
+	const [pools, setPools] = useState([])
+
+	useEffect( () => {
+		// get all our pools from the api
+		// update pools in our state
+		// use Axios  yarn add axios
+
+		axios.get('/api/v1/pools.json')
+		.then( resp => console.log(resp) )
+		.catch( resp => console.log(resp) )
+	}, [pools.length])
+
+	return (
+		<div>This is the Pools#index view for our app.</div>
+	)
+
 }
+
+
 
 export default Pools
