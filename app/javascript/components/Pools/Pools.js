e598ab326de6c975cf24632aa43f45accf167caf
Peter <peter@ptorrsmith.com>
2020-09-27 22:30:01 +1300

    Styled components for Pools and PoolsListItem

--- app/javascript/components/Pools/Pools.js
+++ app/javascript/components/Pools/Pools.js
@@ -1,6 +1,37 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
 import PoolsListItem from './PoolsListItem'
+import styled from 'styled-components'
+
+const Home = styled.div`
+	text-align: center;
+	max-width: 1200px;
+	margin-left: auto;
+	margin-right: auto;
+`
+
+const Header = styled.div`
+	padding: 100px 100px 10px 0;
+
+	h1 {
+		font-size: 42px;
+	}
+
+`
+
+const Subheader = styled.div`
+	font-weight: 300;
+	font-size: 26px;
+`
+
+const Grid = styled.div`
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+	grid-gap: 20px;
+	width: 100%;
+	padding: 20px;
+`
+
 
 // stateless component
 // const Pools = () => {
@@ -32,15 +63,15 @@ const Pools = () => {
 
 	return (
 		// <Fragment>  // use a div for now
-		<div className="home">
-			<div className='header'>
+		<Home>
+			<Header>
 				<h1>Pool Reviews</h1>
-				<div className="subheader">Find a good place to swim</div>
-			</div>
-			<div className="grid">
+				<Subheader>Find a good place to swim</Subheader>
+			</Header>
+			<Grid>
 				{grid}
-			</div>
-		</div>
+			</Grid>
+		</Home>
 		// </Fragment>
 
 	)
--- app/javascript/components/Pools/PoolsListItem.js
+++ app/javascript/components/Pools/PoolsListItem.js
@@ -1,19 +1,61 @@
 import React from 'react'
 import {BrowserRouter as Router, Link} from 'react-router-dom'
+import styled from 'styled-components'
+
+const Card = styled.div`
+	border: 1px solid #efefef;
+	background: #fff;
+	text-align: center;
+`
+
+const PoolLogo = styled.div`
+	width: 50px;
+	text-align: center;
+	margin-left: auto;
+	margin-right: auto;
+	padding-top: 10px;
+
+	img {
+		height: 50px;
+		width: 50px;
+		border-radius: 100%;
+		border: 1px solid #efefef
+	}
+`
+
+const PoolName = styled.div`
+	padding: 20px 0 10px 0;
+`
+
+const LinkWrapper = styled.div`
+	margin: 30px 0 20px 0;
+	height: 50px;
+
+	a {
+		color: #fff;
+		background: #000;
+		border-radius: 4px;
+		padding: 10px 50px;
+		border: 1px solid #000;
+		width: 100%;
+		text-decoration: none;
+	}
+`
+
 
 const PoolsListItem = (props) => {
 	return (
-		<div className="card">
-			<div className="pool-logo">
+		<Card>
+			<PoolLogo>
 				<img src={props.attributes.image_url} alt={props.attributes.name} />
-			</div>
-			<div className="pool-name">{props.attributes.name}</div>
+			</PoolLogo>
+			<PoolName>{props.attributes.name}</PoolName>
 			<div className="pool-score">{props.attributes.avg_score}</div>
-			<div className="pool-link">
+			<LinkWrapper>
 				{/* <a href={`/pools/${props.attributes.slug}`}>View {props.attributes.name}</a> */}
-				<Link to={`/pools/${props.attributes.slug}`}>View {props.attributes.name}</Link>
-			</div>
-		</div>
+				<Link to={`/pools/${props.attributes.slug}`}>View pool</Link>
+			</LinkWrapper>
+		</Card>
 	)
 }
 
