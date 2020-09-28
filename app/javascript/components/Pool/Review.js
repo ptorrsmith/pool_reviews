556c3b91d0eae44327a36471e149ee2b01d5eff5
Peter <peter@ptorrsmith.com>
2020-09-28 19:32:08 +1300

    Review component

--- app/javascript/components/Pool/Review.js
+++ app/javascript/components/Pool/Review.js
@@ -1,17 +1,47 @@
 import React from 'react'
+import styled from 'styled-components'
+import Rating from '../Rating/Rating'
+
+const Card = styled.div`
+	border: 1px solid rgba(0,0,0,0.1);
+	border-radius: 4px;
+	padding: 20px;
+	margin: 0 20px 20px 0;
+`
+
+const RatingContainer = styled.div`
+
+`
+
+const RatingScore = styled.div`
+
+`
+
+const Title = styled.div`
+	padding: 20px 0 0 0;
+	font-size: 18px;
+
+`
+
+const Description = styled.div`
+	padding: 0 0 20px 0;
+	font-size: 14px;
+`
+
 
 
 const Review = (props) => {
 	const {score, title, description} = props.attributes
 	return(
-	<div className='card'>
-		<div className='rating-container'>
-	<div className='rating-score'>{score}</div>
-		</div>
-		<div className='title'>{title}</div>
-		<div className='description'>{description}</div>
+	<Card>
+		<RatingContainer>
+			<Rating></Rating>
+			<RatingScore>{score}</RatingScore>
+		</RatingContainer>
+		<Title>{title}</Title>
+		<Description>{description}</Description>
 
-	</div>
+	</Card>
 	)
 }
 
