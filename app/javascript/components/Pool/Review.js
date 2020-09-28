f6d3f73973b8e09286d972c0ad21fdde6779c1a9
Peter <peter@ptorrsmith.com>
2020-09-28 23:38:01 +1300

    Review item and use Rating component (FA stars not working)

--- app/javascript/components/Pool/Review.js
+++ app/javascript/components/Pool/Review.js
@@ -3,40 +3,37 @@ import styled from 'styled-components'
 import Rating from '../Rating/Rating'
 
 const Card = styled.div`
-	border: 1px solid rgba(0,0,0,0.1);
-	border-radius: 4px;
-	padding: 20px;
-	margin: 0 20px 20px 0;
+  border-radius: 4px;
+  border: 1px solid #E6E6E6;
+  padding: 20px;
+  margin: 0px 0px 20px 0px;
+  position: relative;
+  margin-right: 12px;
 `
 
 const RatingContainer = styled.div`
-
-`
-
-const RatingScore = styled.div`
-
+  display: flex;
+  flex-direction: row;
 `
 
 const Title = styled.div`
-	padding: 20px 0 0 0;
-	font-size: 18px;
-
+  padding: 20px 0px 0px 0px;
+  font-family: 'Poppins-Bold';
+  font-size: 18px;
 `
 
 const Description = styled.div`
-	padding: 0 0 20px 0;
-	font-size: 14px;
+  padding: 0 0 20px 0;
+  font-size: 14px;
 `
-
-
 
 const Review = (props) => {
 	const {score, title, description} = props.attributes
+	// console.log('score: ', score)
 	return(
 	<Card>
 		<RatingContainer>
-			<Rating></Rating>
-			<RatingScore>{score}</RatingScore>
+			<Rating score={score}/>
 		</RatingContainer>
 		<Title>{title}</Title>
 		<Description>{description}</Description>
