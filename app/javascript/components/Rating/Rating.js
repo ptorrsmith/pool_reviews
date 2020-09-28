0e22de5d723699cf9e98c2808a16725f3cd3fd9e
Peter <peter@ptorrsmith.com>
2020-09-28 19:32:21 +1300

    initial star rating component

---
+++ app/javascript/components/Rating/Rating.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Rating = (props) => {
+
+	return (
+		<span className='star-wrapper'>
+			<span className='stars'>* * * * *</span>
+		</span>
+	)
+
+}
+
+export default Rating
