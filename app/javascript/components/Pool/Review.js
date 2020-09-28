195f1367f16990197cc3f5d21458dababdae9f21
Peter <peter@ptorrsmith.com>
2020-09-28 19:11:03 +1300

    Initial Review component

--- app/javascript/components/Pool/Review.js
+++ app/javascript/components/Pool/Review.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 
 const Review = (props) => {
+	const {score, title, description} = props.attributes
 	return(
-	<div>hi</div>
+	<div className='card'>
+		<div className='rating-container'>
+	<div className='rating-score'>{score}</div>
+		</div>
+		<div className='title'>{title}</div>
+		<div className='description'>{description}</div>
+
+	</div>
 	)
 }
 
